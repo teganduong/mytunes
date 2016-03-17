487a27f7b93f6604f6af44d8037bae956895d6b0
teganduong <teganduong@gmail.com>
2016-03-16 20:08:16 -0700

    Implement songQueue stuff

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,21 @@
 var SongQueue = Songs.extend({
 
   initialize: function() {
+  },
+
+  add: function() {
+      debugger;
+    this.enqueue();
+    if (this.length === 1) {
+      this.playFirst();
+    }
+  },
+
+  playFirst: function() {
+    var firstSong = this.at(0);
+    if (firstSong) {
+      firstSong.play();
+    }
   }
 
 });
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,21 @@ var SongModel = Backbone.Model.extend({
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  // Create enqueue method that triggers enqueue event
+  enqueue: function() {
+    this.trigger('enqueue', this);
+  },
+
+  // Create dequeue method that triggers dequeue event
+  dequeue: function() {
+    this.trigger('dequeue', this);
+  },
+
+  // Create ended method that triggers ended event
+  ended: function() {
+    this.trigger('ended', this);
   }
 
 });
